9c0697f07d2993999040e3c5833eb5711c4007d9
Aflal BK <aflalbk2k2@gmail.com>
2023-09-06 18:04:17 +0530

    Base of choose_your_career project completed, created list of 100 occupations, print one random occupation from that list.

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+# list of 100 Occupations.
+occupations_list = ['Teacher', 'Doctor', 'Engineer', 'Chef', 'Police Officer', 'Nurse', 'Artist', 'Lawyer',
+                    'Accountant', 'Firefighter', 'Writer', 'Electrician', 'Dentist', 'Architect', 'Musician',
+                    'Scientist', 'Pharmacist', 'Plumber', 'Actor', 'Psychologist', 'Librarian', 'Waiter/Waitress',
+                    'Farmer', 'Pilot', 'Photographer', 'Software Developer', 'Hairdresser', 'Carpenter',
+                    'Graphic Designer', 'Journalist', 'Veterinarian', 'Barista', 'Baker', 'Salesperson', 'Chef',
+                    'Electrician', 'Geologist', 'Makeup Artist', 'Astronomer', 'Paramedic', 'Real Estate Agent',
+                    'Biologist', 'Pediatrician', 'Social Worker', 'Mechanic', 'Flight Attendant', 'Historian',
+                    'Archaeologist', 'Landscaper', 'Surveyor', 'Translator', 'Welder', 'Chemist', 'Mathematician',
+                    'Optometrist', 'Radiologist', 'Pharmacist', 'Financial Analyst', 'Archaeologist',
+                    'Speech Therapist', 'Event Planner', 'Fashion Designer', 'Zoologist', 'Economist',
+                    'Archaeologist', 'News Anchor', 'Park Ranger', 'Electrician', 'Accountant',
+                    'Social Media Manager', 'Surveyor', 'Paramedic', 'Forensic Scientist', 'Flight Instructor',
+                    'Wedding Planner', 'Marine Biologist', 'Economist', 'Librarian', 'Radiologic Technologist',
+                    'Veterinarian', 'Acupuncturist', 'Graphic Designer', 'Landscaper', 'Nurse Practitioner',
+                    'Architect', 'Dental Hygienist', 'Actuary', 'Biomedical Engineer', 'Computer Programmer',
+                    'Personal Trainer', 'Environmental Scientist', 'Physical Therapist', 'Astronomer', 'Hairdresser',
+                    'Chiropractor', 'Police Detective', 'Human Resources Manager', 'Museum Curator',
+                    'Occupational Therapist', 'Speech Pathologist']
+
+# print a random Occupation from the list
+print(random.choice(occupations_list))
